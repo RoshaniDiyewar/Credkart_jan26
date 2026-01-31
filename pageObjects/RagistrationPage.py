4c801aa5303121024489f70dc40ec0008db7936e
RoshaniDiyewar <roshanidiyewar@gmail.com>
2026-01-31 07:13:49 +0530

    This is my first commit for Credkart Project

---
+++ pageObjects/RagistrationPage.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+from pageObjects.LoginPage import Login_Page_Class
+
+
+class Registration_Page_Class(Login_Page_Class):
+
+    text_name_xpath = "//input[@id='name']"
+    text_confirm_password_xpath = "//input[@id='password-confirm']"
+
+
+    def Enter_Name(self, name):
+        self.driver.find_element(By.XPATH, self.text_name_xpath).send_keys(name)
+
+    def Enter_Confirm_Password(self, password):
+        self.driver.find_element(By.XPATH, self.text_confirm_password_xpath).send_keys(password)
