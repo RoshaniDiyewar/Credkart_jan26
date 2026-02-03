ac740eaefd92c6ebf443a392d5a2a1a064f9aaf9
RoshaniDiyewar <roshanidiyewar@gmail.com>
2026-02-03 12:38:33 +0530

    edited

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-pytest -v -s -n=auto --html=Html_reports\my_headlessreport_31th_jan_2026.html --browser headless
+pytest -v -s -n=auto --html=Html_reports\my_headlessreport_31th_jan_2026.html --browser headless "--disable-warnings"
