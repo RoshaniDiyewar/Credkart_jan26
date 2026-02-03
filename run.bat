e8c9a7f5214dcbfc4e263fe785c28bafaada4f13
RoshaniDiyewar <roshanidiyewar@gmail.com>
2026-02-03 08:28:10 +0530

    edited

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-pytest -v -s -n=auto --html=Html_reports\my_chromereport_28th_jan_2026.html --browser chrome
-pytest -v -s -n=auto --html=Html_reports\my_chromereport_28th_jan_2026.html --browser firefox
+pytest -v -s -n=auto --html=Html_reports\my_headlessreport_31th_jan_2026.html --browser headless -k "test_verify_Credkart_url_001"
+pytest -v -s -n=auto --html=Html_reports\my_firefoxreport_31th_jan_2026.html --browser firefox -k "test_verify_Credkart_url_001"
+pytest -v -s -n=auto --html=Html_reports\my_chromereport_31th_jan_2026.html --browser chrome -k "test_verify_Credkart_url_001"
+pytest -v -s -n=auto --html=Html_reports\my_edgereport_31th_jan_2026.html --browser edge -k "test_verify_Credkart_url_001"
